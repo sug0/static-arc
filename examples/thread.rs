b5aeb6d8f18e3754b17d55d5470ecdd83846e7b1
Tiago Carvalho <sugoiuguu@tfwno.gf>
2022-03-25 15:24:15 +0000

    add an example

---
+++ examples/thread.rs
@@ -0,0 +1,47 @@
+use std::thread;
+use std::sync::Mutex;
+use std::collections::HashMap;
+
+use static_arc::StaticArc;
+
+struct SharedData {
+    data: Mutex<HashMap<String, i32>>,
+}
+
+fn main() {
+    let [thread_ptr, main_ptr] = StaticArc::new(SharedData {
+        data: Mutex::new(HashMap::new()),
+    }).unwrap();
+
+    thread::spawn(move || {
+        let mut data = thread_ptr.data.lock().unwrap();
+        data.insert("x".into(), 1);
+        data.insert("y".into(), 2);
+        data.insert("z".into(), 3);
+        println!("thang 1");
+    });
+
+    println!("thang 2");
+    let data = recover(main_ptr);
+    println!("thang 3");
+
+    for (k, v) in data.iter() {
+        println!("{}: {}", k, v);
+    }
+}
+
+fn recover(arc: StaticArc<SharedData>) -> HashMap<String, i32> {
+    let mut opt_arc = Some(arc);
+
+    loop {
+        let result = opt_arc
+            .take()
+            .unwrap()
+            .try_into_inner_recover();
+
+        match result {
+            Ok(shared) => return shared.data.into_inner().unwrap(),
+            Err(arc) => opt_arc = Some(arc),
+        }
+    }
+}
